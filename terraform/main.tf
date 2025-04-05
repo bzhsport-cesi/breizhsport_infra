1da2eb5ffb5f2bf33567d44a25d10414a8e43426
Théo Dauzon <theo.dauzon@gmail.com>
2025-04-05 18:01:32 +0200

    Add Terraform configuration for VM creation, public IP assignment, and Ansible inventory generation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,55 @@
+###############################################################################
+# 1) Réseau
+###############################################################################
+
+data "warren_network" "public" {
+  name = var.network_name
+}
+
+###############################################################################
+# 2) Création de la VM
+###############################################################################
+
+resource "warren_virtual_machine" "denvr_vm" {
+  count             = var.vm_number
+  disk_size_in_gb   = var.disk_size
+  memory            = var.ram_number
+  name              = "${var.vm_prefix}-${count.index}"
+  username          = var.username
+  os_name           = var.os_name
+  os_version        = var.os_version
+  vcpu              = var.cpu_number
+  network_uuid      = data.warren_network.public.id
+  reserve_public_ip = false
+  public_key        = var.ssh_public_key
+}
+
+###############################################################################
+# 3) Attribution d’IP publique
+###############################################################################
+
+resource "warren_floating_ip" "denvr_ip" {
+  count       = var.vm_number
+  name        = "ip-${var.vm_prefix}-${count.index}"
+  assigned_to = warren_virtual_machine.denvr_vm[count.index].id
+}
+
+###############################################################################
+# 4) Génération d’un inventaire Ansible
+###############################################################################
+
+resource "local_file" "ansible_inventory" {
+  content = templatefile("${path.module}/inventory.tmpl", {
+    public_ips = warren_floating_ip.denvr_ip.*.address
+    user       = var.username
+  })
+  filename = "${path.module}/rendered_inventory"
+}
+
+###############################################################################
+# 5) Provisioning Ansible
+###############################################################################
+
 resource "null_resource" "ansible_provision" {
   depends_on = [
     warren_floating_ip.denvr_ip,
@@ -11,21 +63,24 @@ resource "null_resource" "ansible_provision" {
     host        = warren_floating_ip.denvr_ip[0].address
   }
 
+  # Copie de l’inventaire généré
   provisioner "file" {
     source      = local_file.ansible_inventory.filename
     destination = "/tmp/inventory"
   }
 
+  # Copie du playbook et du template Docker Compose
   provisioner "file" {
-    source      = "../playbook.yml"
+    source      = "${path.module}/../playbook.yml"
     destination = "/tmp/playbook.yml"
   }
 
   provisioner "file" {
-    source      = "../templates/docker-compose.yml.j2"
+    source      = "${path.module}/../templates/docker-compose.yml.j2"
     destination = "/tmp/docker-compose.yml.j2"
   }
 
+  # Exécution du playbook Ansible
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update -yq && sudo apt-get install -yq ansible python3-pip",
